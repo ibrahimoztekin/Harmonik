45b52043f4d140c83d34367191c236d8bc2f95bc
ibrahim <ioztkn@gmail.com>
2021-06-26 20:16:24 +0300

    Java ile girilen sayının harmonik serisini bulan program

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int number;
+        double total=0.0;
+
+        Scanner input= new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        number=input.nextInt();
+
+        for(int i=1; i<=number;i++){
+
+            total+=(1.0/i);
+        }
+
+        System.out.println("Harmonik Serisi: "+total);
+    }
+}
